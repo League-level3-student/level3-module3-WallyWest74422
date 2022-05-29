6061319d805f42e3bfb11ee20c843ced821aa2d1
league <league@imac-22.attlocal.net>
2022-05-29 16:28:56 -0700

    yfuhgbfvdx

--- src/_02_The_Wave/_01_TheWave.java
+++ src/_02_The_Wave/_01_TheWave.java
@@ -20,7 +20,11 @@ public class _01_TheWave {
      */
     
     public static ArrayList<String> wave(String str) {
-        
+        String[] strArray = str.split("");
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < strArray.length; i++) {
+			String s = strArray[i].toUpperCase();
+		}
         return null;
     }
 }
